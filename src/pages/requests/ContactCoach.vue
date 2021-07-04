c6f15712a45a250dd17bce3cec2c92cb8dcb5a90
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-04 15:13:15 +0530

    create routes

---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: 'ContactCoach'
+  };
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,19 @@
+import CoachesList from './pages/coaches/CoachesList';
+import CoachDetails from './pages/coaches/CoachDetails';
+import ContactCoach from './pages/requests/ContactCoach';
+import CoachRegistration from './pages/coaches/CoachRegistration';
+import RequestsReceived from './pages/requests/RequestsReceived';
+import NotFound from './pages/NotFound';
 
 const routes = [
   {path:'/',redirect:'/coaches'},
-  {path:'/coaches',component:null},
-  {path:'/coaches/:id',component:null,children:[
-      {path:'contact',component:null},
+  {path:'/coaches',component:CoachesList},
+  {path:'/coaches/:id',component:CoachDetails,children:[
+      {path:'contact',component:ContactCoach},
   ]},
-  {path:'/register',component:null},
-  {path:'/request',component:null},
-  {path:'/:notFound(.*)',component:null}
+  {path:'/register',component:CoachRegistration},
+  {path:'/request',component:RequestsReceived},
+  {path:'/:notFound(.*)',component:NotFound}
 ];
 
 const router = VueRouter.createRouter({

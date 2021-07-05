fd84f5367b4ecdca6d8a684bed9d467814bc09d5
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-05 09:53:51 +0530

    create store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
-import Vuex from 'vuex';
+//import Vue from 'vue'
+import { createStore } from 'vuex'
+
+//Vue.use(Vuex)
 
 import coachesModule from './modules/coaches/index';
 
-const store = Vuex.Store({
+const store = createStore({
   modules:{
     coaches: coachesModule
   }
 })
+
+export default store;

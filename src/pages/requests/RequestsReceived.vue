72b46c04f88ef993ea421a8726c3465d8e500620
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-10 11:33:15 +0530

    implement requests view

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -5,7 +5,14 @@
                 <h2>Received Requests</h2>
             </header>
             <ul v-if="hasRequests">
+                <request-item
+                        v-for="request in receivedRequests"
+                        :key="request.id"
+                        :email="request.userEmail"
+                        :message="request.message"
+                >
 
+                </request-item>
             </ul>
             <h3 v-else>You have not received any requests yet!</h3>
         </base-card>
@@ -13,8 +20,13 @@
 </template>
 
 <script>
+  import RequestItem from '../components/requests/RequestItem';
+
   export default {
     name: 'RequestsReceived',
+    components:{
+      RequestItem
+    },
     computed:{
       receivedRequests(){
         return this.$store.getters['requests/requests'];

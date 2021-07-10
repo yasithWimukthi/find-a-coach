e58de9e217f40b57fc8e7a4910b45240cf2a4fe2
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-10 11:21:15 +0530

    implement requests view

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,13 +1,44 @@
 <template>
-    requests
+    <section>
+        <base-card>
+            <header>
+                <h2>Received Requests</h2>
+            </header>
+            <ul v-if="hasRequests">
+
+            </ul>
+            <h3 v-else>You have not received any requests yet!</h3>
+        </base-card>
+    </section>
 </template>
 
 <script>
   export default {
-    name: 'RequestsReceived'
+    name: 'RequestsReceived',
+    computed:{
+      receivedRequests(){
+        return this.$store.getters['requests/requests'];
+      },
+      hasRequests(){
+        return this.$store.getters['requests/hasRequest'];
+      }
+    }
   };
 </script>
 
 <style scoped>
+    header {
+        text-align: center;
+    }
 
+    ul {
+        list-style: none;
+        margin: 2rem auto;
+        padding: 0;
+        max-width: 30rem;
+    }
+
+    h3 {
+        text-align: center;
+    }
 </style>

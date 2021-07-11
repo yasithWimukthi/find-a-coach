eaf88522ce9a239fc13f339425ba9f01c17a63e2
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-11 11:29:09 +0530

    get data from firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store/index';
 import BaseCard from './pages/components/ui/BaseCard';
 import BaseButton from './pages/components/ui/BaseButton';
 import BaseBadge from './pages/components/ui/BaseBadge';
+import BaseSpinner from './pages/components/ui/BaseSpinner';
 
 const app = createApp(App);
 app.use(router);
@@ -12,4 +13,5 @@ app.use(store);
 app.component('base-card',BaseCard);
 app.component('base-button',BaseButton);
 app.component('base-badge',BaseBadge);
+app.component('base-spinner',BaseSpinner)
 app.mount('#app');

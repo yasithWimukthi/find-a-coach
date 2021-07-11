5e08100f34e734075f214e4c234b5a13ff355304
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-11 11:11:05 +0530

    get data from firebase

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
     <section>
         <base-card>
             <div class="controls">
-                <base-button mode="outline">Refresh</base-button>
+                <base-button mode="outline" @click="loadCoaches">Refresh</base-button>
                 <base-button v-if="!isCoach" link to="/register" >Register as Coach</base-button>
             </div>
             <ul v-if="hasCoaches">
@@ -60,7 +60,13 @@
     methods:{
       setFilters(updatedFilters){
         this.activeFilters = updatedFilters;
+      },
+      loadCoaches(){
+        this.$store.dispatch('coaches/loadCoaches');
       }
+    },
+    created() {
+      this.loadCoaches();
     }
   };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -11,6 +11,7 @@ export default {
       hourlyRate: data.rate
     };
 
+    /**write data into firebase**/
     const response = await fetch(`https://vue-find-coach-23ff7-default-rtdb.firebaseio.com/coaches/${userId}.json`,{
       method:'PUT',
       body:JSON.stringify(coachData)
@@ -26,5 +27,33 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+
+  /*** get data from firebase*/
+  async loadCoaches(context){
+    const response = await fetch(`https://vue-find-coach-23ff7-default-rtdb.firebaseio.com/coaches.json`);
+    const responseData = await response.json();
+
+    if(!response.ok){
+      //error
+    }
+
+    const coaches = [];
+
+    for(const key in responseData){
+      const coach = {
+        id:key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        areas: responseData[key].areas,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].rate
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches',coaches);
+
   }
+
 }

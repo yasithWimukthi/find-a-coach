e55d49eb77989fe1b4e67c639f9e79ec466adea2
yasithWimukthi <it19966922@my.sliit.lk>
2021-07-11 10:28:43 +0530

    store data in firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,30 @@
 export default {
-  registerCoach(context,data){
+  async registerCoach(context,data){
+
+    const userId = context.rootGetters.userId;
+
     const coachData = {
-      id:"coach_3",
       firstName: data.firstName,
       lastName: data.lastName,
       areas: data.areas,
       description: data.description,
       hourlyRate: data.rate
     };
-    context.commit('registerCoach',coachData);
+
+    const response = await fetch(`https://vue-find-coach-23ff7-default-rtdb.firebaseio.com/coaches/${userId}.json`,{
+      method:'PUT',
+      body:JSON.stringify(coachData)
+    });
+
+    const responseData = await response.json();
+
+    if(!response.ok){
+      //error
+    }
+
+    context.commit('registerCoach',{
+      ...coachData,
+      id: userId
+    });
   }
 }
